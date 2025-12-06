516c7ac28ad5a89a07ab5c80d510e0d09c9e996b
Alexander Engberg <alexander.engberg@gmail.com>
2025-12-06 20:51:09 +0100

    restart

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,9 +1,10 @@
-using Web;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.AddWebServices();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
@@ -11,6 +12,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("/api", (options) =>
+    {
+        options.Title = "Just another app API";
+        options.Layout = ScalarLayout.Modern;
+        options.HideClientButton = true;
+    });
 }
 
 app.UseHttpsRedirection();
--- src/Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ src/Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b016e173b09864842933f006391df70d57d3bbbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
